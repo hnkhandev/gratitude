ce5fd0c690c8bcc8e70124565fb3cee343b7e98e
hnkhandev <hnkhandev@gmail.com>
2023-09-05 13:09:28 -0600

    move sign-in redirect to middleware

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,15 +1,7 @@
-import { authConfig } from "@/auth/auth-config";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import WhopSignIn from "@/components/whop-sign-in";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function SignIn() {
-  const session = await getServerSession(authConfig);
-  if (session) {
-    redirect("/dashboard");
-  }
-
   return (
     <main className="container flex flex-col items-center justify-center flex-1 gap-4 sm:px-0">
       <Card className="w-full max-w-md">
